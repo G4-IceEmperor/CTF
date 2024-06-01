20e3d73fb42aea239657a8a5f9434a799b556d2d
Bian Yuxuan Jonathan <128063701+G4-IceEmperor@users.noreply.github.com>
2024-06-01 21:45:19 +0800

    Update main.py

--- Nahamcon 2024/base3200/main.py
+++ Nahamcon 2024/base3200/main.py
@@ -1,1 +1,23 @@
+import base64
+import sys
+
+def decode_file(input_file, output_file='output.txt', iterations=50):
+    with open(input_file, 'rb') as f:
+        encoded_content = f.read()
+
+    decoded_content = encoded_content
+    for _ in range(iterations):
+        decoded_content = base64.b64decode(decoded_content)
+
+    with open(output_file, 'wb') as f:
+        f.write(decoded_content)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python main.py input_file")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+
+    decode_file(input_file)
 
